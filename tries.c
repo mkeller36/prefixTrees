49b99438aa0169ec888cfe1f186c7dd9334395c2
mkeller@arch <michaelkeller1997@gmail.com>
2025-01-26 19:54:28 -0500

    Added create node function

--- tries.c
+++ tries.c
@@ -1,7 +1,27 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
+#define NUM_CHAR 256
+
+typedef struct trienode{
+  struct trienode *children[NUM_CHAR];
+  bool terminating;
+} trienode;
+
+trienode *createnode(){
+  trienode *newnode = malloc(sizeof(*newnode));
+
+  for(int i = 0; i < NUM_CHAR; i++){
+    newnode->children[i] = NULL;
+  } 
+  newnode->terminal = false;
+  return newnode;
+}
 
 void main(void){
-
+  
+  
 
 }
