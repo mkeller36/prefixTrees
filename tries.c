0c567c2e3a7707742d586690b7ea8081254bc5b1
mkeller@arch <michaelkeller1997@gmail.com>
2025-03-05 19:28:19 -0500

    Added till end of video 1

--- tries.c
+++ tries.c
@@ -16,7 +16,7 @@ trienode *createnode(){
   for(int i = 0; i < NUM_CHAR; i++){
     newnode->children[i] = NULL;
   } 
-  newnode->terminal = false;
+  newnode->terminating = false;
   return newnode;
 }
 
@@ -25,7 +25,7 @@ bool trieInsert(trienode **root, char *signedtext){
     *root = createnode();
   }
 
-  unisgned char *text = (unsigned char *)signedtext;
+  unsigned char *text = (unsigned char *)signedtext;
   trienode *tmp = *root;
 
   int length = strlen(signedtext);
@@ -34,12 +34,53 @@ bool trieInsert(trienode **root, char *signedtext){
     if(tmp->children[text[i]] == NULL){
       /* Create new node */
       tmp->children[text[i]] = createnode();
+
+    }
+    tmp = tmp->children[text[i]];
+  }
+
+  if(tmp->terminating){
+    return false;
+  }
+  else{
+    tmp->terminating = true;
+    return true;
+  }
+}
+
+void printftrie_rec(trienode *node, unsigned char * prefix, int length){
+  unsigned char newprefix[length+2];
+  memcpy(newprefix, prefix, length);
+  newprefix[length+1] = 0;
+
+  if(node->terminating){
+    printf("WORD: %s\n", prefix);
+  }
+
+  for(int i = 0; i < NUM_CHAR; i++){
+    if(node->children[i] != NULL){
+      newprefix[length] = i;
+      printftrie_rec(node->children[i], newprefix, length+1);
     }
   }
 }
 
+void printtrie(trienode * root){
+  if (root == NULL){
+    printf("TRIE empty\n");
+    return;
+  }
+  printftrie_rec(root, NULL, 0);
+}
+
 void main(void){
-  
+  trienode *root = NULL;
+  trieInsert(&root, "KIT");
+  trieInsert(&root, "CATTLE");
+  trieInsert(&root, "KIN");
+  trieInsert(&root, "CAT");
+  trieInsert(&root, "HAPPY");
+  printtrie(root);
   
 
-}
+} 
